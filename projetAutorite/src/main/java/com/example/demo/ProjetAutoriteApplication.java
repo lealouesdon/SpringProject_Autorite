0a62e10809c49f7226bffe31461d755f15553541
Léa <lea.louesdon@gmail.com>
2019-03-11 16:30:40 +0100

    new project

---
+++ projetAutorite/src/main/java/com/example/demo/ProjetAutoriteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAutoriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAutoriteApplication.class, args);
+	}
+
+}
